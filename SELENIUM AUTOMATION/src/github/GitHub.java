ab3ed181b502be9b502d2ec101b6e21aad73360f
sushilraja <sushilraja@localhost>
2024-11-15 13:34:43 +0530

    modified push

---
+++ SELENIUM AUTOMATION/src/github/GitHub.java
@@ -0,0 +1,14 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHub {
+	
+	@Test
+	public void addMessage()
+	{
+		Reporter.log("WELCOME TO GitHub",true);
+	}
+
+}
